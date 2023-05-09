7545d1acf0715be01473612cb775291df1d2b2e4
Daniel Noun <danielnoun21@gmail.se>
2023-05-09 10:26:56 +0200

    ss

--- Entiteter/Annons.cs
+++ Entiteter/Annons.cs
@@ -12,22 +12,30 @@ namespace Entiteter
     public class Annons
     {
         
-        public Person SkapadAv { get; set; }
+        public int AnnonsId { get; set; }
+        public DateTime AnnonsDatum { get; set; }
 
         public string Kategori;
 
         public string Underkategori;
-        public Klädesplagg Storlek { get; set; }
+
+        public float AnnonsPris { get; set; }
+        public bool Postas { get; set; }
         public Skick Skick { get; set; }
-        public float AnnonsPris {get; set; }
+        public Klädesplagg Storlek { get; set; }
+        public Klädesplagg Rubrik { get; set; }
+
+
+
+        public Person SkapadAv { get; set; }
         public Person Plats { get; set; }
         public Person Kommun { get; set; }
         public Person PostNummer { get; set; }
-        public Klädesplagg TypAvPlagg { get; set; }
+
+        
         
         public Klädesplagg Kön { get; set; }
-        public bool Postas { get; set; }
-
+        
         
     }
 }
